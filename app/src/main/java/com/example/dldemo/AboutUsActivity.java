06ae2743a93c1e566b9baba7b93ef7e1b7df29ce
MatrixcsYounes <younesho9@outlook.com>
2023-03-26 18:36:45 +0000

    About us and feedback(buggy)

--- app/src/main/java/com/example/dldemo/AboutUsActivity.java
+++ app/src/main/java/com/example/dldemo/AboutUsActivity.java
@@ -1,4 +1,53 @@
 package com.example.dldemo;
 
-public class AboutUsActivity {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        String description = getString(R.string.aboutus_description);
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.duolingo)
+                .setDescription(description)// or Typeface
+                .addGroup("Connect with us")
+                .addEmail("rabiiyahya1@gmail.com")
+                .addWebsite("https://yahya.rabii.me/")
+                .addFacebook("https://www.facebook.com")
+                .addTwitter("https://www.twitter.com")
+                .addYoutube("https://www.youtube.com")
+                .addPlayStore("com.ideashower.readitlater.pro")
+                .addGitHub("https://github.com/Yahya-rabii")
+                .addInstagram("https://www.instagram.com/yahya_rabii")
+                .create();
+        setContentView(aboutPage);
+    }
+    private Element createCopyright(){
+        Element copyright = new Element();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
+        final String currentYear = dateFormat.format(new Date());
+        final String copyrightString = String.format("Copyright by Noxidious and Matrixcsyounes %s", currentYear);
+        copyright.setTitle(copyrightString);
+        copyright.setGravity(Gravity.CENTER);
+        return copyright;
+    }
 }
--- app/src/main/java/com/example/dldemo/FeedBackActivity.java
+++ app/src/main/java/com/example/dldemo/FeedBackActivity.java
@@ -1,4 +1,82 @@
 package com.example.dldemo;
 
-public class FeedBackActivity {
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FeedBackActivity extends AppCompatActivity{
+
+    private EditText feedbackEditText;
+    private Button submitButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        //Firebase will be initialized here
+        feedbackEditText = findViewById(R.id.feedbackEditText);
+        submitButton = findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String feedbacktext = feedbackEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(feedbacktext)){
+                    Toast.makeText(FeedBackActivity.this, "Please enter feedback", Toast.LENGTH_SHORT).show();
+                }else {
+                    saveFeedbackToFirebase(feedbacktext);
+                    Toast.makeText(FeedBackActivity.this, "Feedback submitted!", Toast.LENGTH_SHORT).show();
+                    feedbackEditText.setText("");
+
+                }
+            }
+        });
+    }
+    private void saveFeedbackToFirebase(String feedbackText) {
+        DatabaseReference feedbackRef = FirebaseDatabase.getInstance().getReference("feedback");
+        String feedbackId = feedbackRef.push().getKey();
+        Feedback feedback = new Feedback(feedbackId, feedbackText);
+        feedbackRef.child(feedbackId).setValue(feedback);
+    }
+
+    public class Feedback {
+        private String feedbackId;
+        private String feedbackText;
+
+        public Feedback() {
+            // Default constructor required for calls to DataSnapshot.getValue(Feedback.class)
+            this.feedbackId = null;
+            this.feedbackText = null;
+        }
+
+        public Feedback(String feedbackId, String feedbackText) {
+            this.feedbackId = feedbackId;
+            this.feedbackText = feedbackText;
+        }
+
+        public String getFeedbackId() {
+            return feedbackId;
+        }
+
+        public void setFeedbackId(String feedbackId) {
+            this.feedbackId = feedbackId;
+        }
+
+        public String getFeedbackText() {
+            return feedbackText;
+        }
+
+        public void setFeedbackText(String feedbackText) {
+            this.feedbackText = feedbackText;
+        }
+    }
 }
